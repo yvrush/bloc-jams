525cc733042319259364d0d2cf803f1d840b4c00
ruby -v <rails -c>
2017-04-19 18:25:25 -0700

    Adding events to animate points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
+
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
  
      var revealPoint = function(index) {
          points[index].style.opacity = 1;
@@ -14,3 +15,19 @@ var animatePoints = function() {
      revealPoint(i);
      }
  };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
+    
+
