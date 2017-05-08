323f220aa44d6ded9cd2cd9ae055a1259fd1d39d
ruby -v <rails -c>
2017-05-08 03:26:15 -0700

    Assignment fixing getParentByClassName with if conditions
    Signed-off-by: ruby -v <rails -c>

--- scripts/album.js
+++ scripts/album.js
@@ -123,12 +123,20 @@ var setCurrentAlbum = function (album) {
 };
 
 var findParentByClassName = function (element, targetClass) {
-    if (element) {
+    if (element.parentElement == null) {
+        console.log("No parent found")
+    } else if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
+        } 
+        
+        if (currentParent.parentElement == null && currentParent.className !== targetClass ) {
+            console.log("No parent found with that class name")
         }
-        return currentParent;
+        
+        else if ( currentParent.className == targetClass) {
+        return currentParent;}
     }
 };
 
